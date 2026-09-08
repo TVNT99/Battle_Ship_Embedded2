a2a2ebbcedba8860a5f00c48d11e898b69b8e888
TinTran <s3870729@rmit.edu.vn>
2026-09-08 19:30:32 +0700

    fix: Add Font8x16, remove unused EADC touch code for LCD module

--- EBI_LCD_Module.c
+++ EBI_LCD_Module.c
@@ -421,41 +421,9 @@ void LCD_BlankArea(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, uint16_t colo
  */
 uint16_t Get_TP_X(void)
 {
-    uint16_t    x_adc_in;
-    uint16_t    X_pos;
-
-    /*=== Get X from ADC input ===*/
-    PB9 = 1;
-    PH4 = 0;
-    GPIO_SetMode(PB, BIT9, GPIO_MODE_OUTPUT);   // XR
-    GPIO_SetMode(PH, BIT4, GPIO_MODE_OUTPUT);   // XL
-    GPIO_SetMode(PH, BIT5, GPIO_MODE_INPUT);    // YD
-
-    /* Configure the GPB8 ADC analog input pins. */
-    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB8MFP_Msk | SYS_GPB_MFPH_PB9MFP_Msk);
-    SYS->GPB_MFPH |= SYS_GPB_MFPH_PB8MFP_EADC0_CH8;
-
-    /* Disable the GPB8 digital input path to avoid the leakage current. */
-    GPIO_DISABLE_DIGITAL_PATH(PB, BIT8);
-
-    /* Configure the sample module 1 for analog input channel 8 and software trigger source.*/
-    EADC_ConfigSampleModule(EADC, 1, EADC_SOFTWARE_TRIGGER, 8); // YU
-
-    /* Clear the A/D ADINT1 interrupt flag for safe */
-    EADC_CLR_INT_FLAG(EADC, EADC_STATUS2_ADIF1_Msk);
-
-    /* Enable the sample module 1 interrupt. */
-    EADC_ENABLE_INT(EADC, BIT1);    //Enable sample module A/D ADINT1 interrupt.
-    EADC_ENABLE_SAMPLE_MODULE_INT(EADC, 1, BIT1);    //Enable sample module 1 interrupt.
-    NVIC_EnableIRQ(EADC01_IRQn);
-
-    /* Reset the ADC interrupt indicator and trigger sample module 1 to start A/D conversion */
-    g_u32AdcIntFlag_TP = 0;
-    EADC_START_CONV(EADC, BIT1);
-
-    /* Wait ADC interrupt (g_u32AdcIntFlag_TP will be set at IRQ_Handler function) */
-    while(g_u32AdcIntFlag_TP == 0) {};
-    x_adc_in = EADC_GET_CONV_DATA(EADC, 1)>>2;
+    /* Touch screen not implemented for Battle Ship */
+    return 0;
+}
 
     /*=== Calculate the X position ===*/
     X_pos = (x_adc_in - 170)/2.8;	// range of x_adc_in is [0:842]
@@ -476,53 +444,54 @@ uint16_t Get_TP_X(void)
  *
  * @details     To get the Y position when finger touching on the LCD screen
  */
-uint16_t Get_TP_Y(void)
-{
-    uint16_t    y_adc_in;
-    uint16_t    Y_pos;
+uint16_t Get_TP_Y(void) { return 0; }
 
-    /*=== Get Y from ADC input ===*/
-    PB8 = 1;
-    PH5 = 0;
-    GPIO_SetMode(PB, BIT8, GPIO_MODE_OUTPUT);   // YU
-    GPIO_SetMode(PH, BIT5, GPIO_MODE_OUTPUT);   // YD
-    GPIO_SetMode(PH, BIT4, GPIO_MODE_INPUT);    // XL
 
-    /* Configure the GPB9 ADC analog input pins. */
-    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB8MFP_Msk | SYS_GPB_MFPH_PB9MFP_Msk);
-    SYS->GPB_MFPH |= SYS_GPB_MFPH_PB9MFP_EADC0_CH9;
 
-    /* Disable the GPB9 digital input path to avoid the leakage current. */
-    GPIO_DISABLE_DIGITAL_PATH(PB, BIT9);
 
-    /* Configure the sample module 2 for analog input channel 9 and software trigger source.*/
-    EADC_ConfigSampleModule(EADC, 2, EADC_SOFTWARE_TRIGGER, 9); // XR
 
-    /* Clear the A/D ADINT1 interrupt flag for safe */
-    EADC_CLR_INT_FLAG(EADC, EADC_STATUS2_ADIF1_Msk);
 
-    /* Enable the sample module 2 interrupt. */
-    EADC_ENABLE_INT(EADC, BIT2);    //Enable sample module A/D ADINT1 interrupt.
-    EADC_ENABLE_SAMPLE_MODULE_INT(EADC, 1, BIT2);    //Enable sample module 2 interrupt.
-    NVIC_EnableIRQ(EADC02_IRQn);
 
-    /* Reset the ADC interrupt indicator and trigger sample module 2 to start A/D conversion */
-    g_u32AdcIntFlag_TP = 0;
-    EADC_START_CONV(EADC, BIT2);
 
-    /* Wait ADC interrupt (g_u32AdcIntFlag_TP will be set at IRQ_Handler function) */
-    while(g_u32AdcIntFlag_TP == 0) {};
-    y_adc_in = EADC_GET_CONV_DATA(EADC, 2)>>2;
 
-    /*=== Calculate the Y position ===*/
-			Y_pos = (y_adc_in - 150)/2.34; // range of y_adc_in is [0:898.8]
 
-    if(Y_pos >= (LCD_H - 1)) Y_pos = LCD_H - 1;
 
-    printf("Position Y : %d \n", Y_pos);
-    return Y_pos;
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * @brief       ADC01 IRQ handler
